e92f3603e0d6aaf9998aeb9840666a40bb53dcb9
Jaime-Felix <jaime.fm@culiacan.tecnm.mx>
2024-02-28 19:52:57 -0700

    Agregar archivos de proyecto.

---
+++ Examen-Ic/Examen_Ic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_Ic
+{
+    internal class Examen_Ic
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
